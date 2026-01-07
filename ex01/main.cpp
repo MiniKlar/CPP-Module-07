e94e9805d99ae0d588d12a5239015fec816a1a23
MiniKlar <omontloris@gmail.com>
2026-01-07 22:58:38 +0100

    - Refactored main.cpp: commented out unused functions and updated array initialization for consistency

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,30 +6,35 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 22:39:06 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/06 01:06:06 by lomont           ###   ########.fr       */
+/*   Updated: 2026/01/07 22:57:49 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void f(std::string & str) {
-	std::cout << "str function called: " << str << std::endl;
-}
+// void f(std::string & str) {
+// 	std::cout << "str function called: " << str << std::endl;
+// }
 
-void f(const std::string & str) {
-	std::cout << "Const str function called: " << str << std::endl;
-}
+// void f(const std::string & str) {
+// 	std::cout << "Const str function called: " << str << std::endl;
+// }
 
-void ff(const size_t & i) {
-	std:: cout << "const i function called : " << i << std::endl;
-}
+// void ff(const size_t & i) {
+// 	std:: cout << "const i function called : " << i << std::endl;
+// }
+
+// void ff(size_t & i) {
+// 	std:: cout << "i function called : " << i << std::endl;
+// }
 
 int	main( void )
 {
-	const std::string	array[5] = {"Salut", "toi", "ça", "va", "bien"};
-	::iter(array, 5, static_cast<void(*) (const std::string&)> (f));
+	std::string		array[5] = {"One", "Two", "Three", "Four", "Five"};
+	::iter(array, 5, static_cast<void(*) (std::string &)> (f));
 
-	const size_t		arrayS[5] = {1, 2, 3, 4, 5};
-	::iter(arrayS, 5, ff);
+	const size_t	arrayS[5] = {1, 2, 3, 4, 5};
+	::iter(arrayS, 5, static_cast<void(*) (const size_t &)> (f));
+
 	return (0);
 }
