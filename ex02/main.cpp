dba922e82e32c0e556ea42e7d3343f0cd5b1fe98
MiniKlar <omontloris@gmail.com>
2026-01-07 22:59:00 +0100

    - Refactored main.cpp: replaced getSize() calls with size() for consistency and corrected a typo in output message

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 20:22:19 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/07 22:15:45 by lomont           ###   ########.fr       */
+/*   Updated: 2026/01/07 22:33:17 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	main( void ) {
 	try {
 		//good array
 		Array <char, size_t> a1(10);
-		std::cout << a1.getSize()<< std::endl;
-		for (size_t i = 0; i < a1.getSize(); i++) {
+		std::cout << a1.size()<< std::endl;
+		for (size_t i = 0; i < a1.size(); i++) {
 			a1[i] = i + 36;
 		};
-		for (size_t i = 0; i < a1.getSize(); i++) {
+		for (size_t i = 0; i < a1.size(); i++) {
 			std::cout << a1[i] << std::endl;
 		}
 		std::cout << std::endl;
@@ -33,7 +33,7 @@ int	main( void ) {
 		//good array
 		Array <std::string, size_t> a1(10);
 		std::string strArray[10] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
-		std::cout << a1.getSize()<< std::endl;
+		std::cout << a1.size()<< std::endl;
 		for (size_t i = 0; i < 10; i++) {
 			a1[i] = strArray[i];
 		};
@@ -50,7 +50,7 @@ int	main( void ) {
 	std::cout << "Copy constructor test" << std::endl;
 
 	Array<int, size_t> a1(10);
-	for (size_t i = 0; i < a1.getSize(); i++) {
+	for (size_t i = 0; i < a1.size(); i++) {
 		a1[i] = i + 1;
 	}
 
@@ -62,16 +62,16 @@ int	main( void ) {
 
 	Array<int, size_t> a3 = a2;
 	a3[5] = 100;
-	std::cout << "Here is a2[5]= " << a2[5] << " and here is t3[5]= " << a3[5] << std::endl << std::endl;
+	std::cout << "Here is a2[5]= " << a2[5] << " and here is a3[5]= " << a3[5] << std::endl << std::endl;
 	}
 
 	try {
 		//bad array
 		Array<int, size_t> a1(20);
-		std::cout << a1.getSize() << std::endl;
-		for (size_t i = 0; i < a1.getSize() + 1; i++)
+		std::cout << a1.size() << std::endl;
+		for (size_t i = 0; i < a1.size() + 1; i++)
 			a1[i] = i + 1;
-		for (size_t i = 0; i < a1.getSize(); i++)
+		for (size_t i = 0; i < a1.size(); i++)
 			std::cout << a1[i] << std::endl;
 		std::cout << std::endl;
 	} catch (std::exception & e) {
