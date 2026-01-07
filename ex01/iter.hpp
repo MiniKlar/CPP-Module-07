3db58c51c4210e4099fa0dc8172bd0f5a49a612f
MiniKlar <omontloris@gmail.com>
2026-01-07 22:58:32 +0100

    - Refactored iter.hpp: restored function declarations, removed commented-out code, and ensured consistent use of #pragma once

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,27 +6,29 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 22:39:04 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/06 00:55:48 by lomont           ###   ########.fr       */
+/*   Updated: 2026/01/07 22:52:04 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 #ifndef	ITER_HPP
 #define	ITER_HPP
 
 #include <iostream>
 
-// template< typename T>
-// void f( T & str ) {
-// 	std::cout << str << std::endl;
-// }
+template< typename T >
+void f( T & str ) {
+	std::cout << str << std::endl;
+}
 
-// template< typename T>
-// void f( const T & str ) {
-// 	std::cout << str << std::endl;
-// }
+template< typename T >
+void f( const T & str ) {
+	std::cout << "const " << str << std::endl;
+}
 
 template< typename T, typename L, typename F>
-void iter( const T array[], const L & size, F f) {
+void iter( const T array[], const L & size, F f(const T &)) {
 	std::cout << "const iter function called" << std::endl;
 	for (L i = 0; i < size; i++) {
 		f(array[i]);
@@ -34,7 +36,7 @@ void iter( const T array[], const L & size, F f) {
 };
 
 template< typename T, typename L, typename F>
-void iter( T array[], const L & size, F f) {
+void iter( T array[], const L & size, F f(T &)) {
 	for (L i = 0; i < size; i++) {
 		f(array[i]);
 	}
